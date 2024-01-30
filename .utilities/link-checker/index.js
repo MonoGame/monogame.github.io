4dcdf196dc9b241b3783d9e2ddb85fc8c5537cb2
Christopher Whitley <aristurtledev@gmail.com>
2024-01-30 01:01:01 -0500

    link check added, found issues in main repo documentation

--- .utilities/link-checker/index.js
+++ .utilities/link-checker/index.js
@@ -1,11 +1,34 @@
 'use strict'
 const linkChecker = require('./linkChecker');
 const path = require('path');
-const fs = require('fs');
 
+//-----------------------------------------------------------------------------
+//  Root Directory is set based on the current working directory as this is
+//  expected to only be run in GitHub Actions CI environment to prevent build
+//  on errors.
+//
+//  When running locally, ensure that your current working directory in your
+//  terminal is the project root directory.
+//-----------------------------------------------------------------------------
 const rootDir = path.join(process.cwd(), '_site', path.sep)
-if(fs.existsSync(rootDir)) {
-    linkChecker(rootDir);
-} else {
-    console.log(`Unable to find site directory at ${rootDir}.  Did you forget to build the site first?`);
-}
+
+linkChecker(rootDir, function (result) {
+    //  Log errors to console.
+    result.errors.forEach(error => {
+        console.error(`${error.reason} from ${error.source} to ${error.target}`)
+    })
+
+    //  Log statistics to console
+    console.log('')
+    console.log('Stats:');
+    console.log(`  Internal Links: ${result.internalLinks}`);
+    console.log(`  Internal Anchored Links: ${result.internalAnchoredLinks}`);
+    console.log(`  Internal Parent Links: ${result.internalParentLinks}`);
+    console.log(`  Internal Parent Anchored Links: ${result.internalParentAnchoredLinks}`);
+    console.log(`  External Links: ${result.externalLinks}`);
+    console.log(`  Error Count: ${result.errors.length}`);
+
+    //  Use the number of errors as the exit code. Anything other than 0 should tell the
+    //  CI environment that the build cannot continue.
+    process.exit(result.errors.length);
+});
--- .utilities/link-checker/linkChecker.js
+++ .utilities/link-checker/linkChecker.js
@@ -1,85 +1,368 @@
-"use strict";
+'use strict';
 
-const fs = require("fs");
-const path = require("path");
-const cheerio = require("cheerio");
-const https = require("https");
-const { URL } = require("url");
+const fs = require('fs')
+const path = require('path')
+const debug = require('debug')('linkchecker')
+const cheerio = require('cheerio')
+const axios = require('axios');
 
-const alreadyChecked = new Set();
-const isValid = new Set();
-const errors = [];
 
-function markLinkInFileInvalid(file, link, lineNumber) {
-    errors.push({
-        file: file,
-        link: link,
-        lineNumber: lineNumber,
-    });
+/**
+ * Checks whether a given href is an internal link.
+ *
+ * @param {string} href - The href to be checked.
+ * @returns {boolean} - True if the href is internal, false otherwise.
+ */
+function isInternalHref(href) {
+    return !href.startsWith('http://') &&
+        !href.startsWith('https://');
 }
 
-function validateFile(rootDir, file) {
-    const absolutePath = path.join(rootDir, file);
-    const html = fs.readFileSync(absolutePath, "utf-8");
-    const $ = cheerio.load(html, { withStartIndices: true });
-    const links = $("a");
+/**
+ * Checks whether a given href is an external link.
+ *
+ * @param {string} href - The href to be checked.
+ * @returns {boolean} - True if the href is external, false otherwise.
+ */
+function isExternalHref(href) {
+    return !isInternalHref(href);
+}
 
-    links.each((index, element) => {
-        const start = $(element).get(0).startIndex;
-        const lineNumber = html.substring(0, start).split("\n").length;
-        const href = $(element).attr("href");
+/**
+ * Formats hrefs, making them consistent and handling special cases.
+ *
+ * @param {string} href - The href to be formatted.
+ * @returns {string} - The formatted href.
+ */
+function formatHref(href) {
+    //  Internal hrefs end with '/' which points to an '/index.html' file, so we need
+    //  to format the local hrefs to include the 'index.html' where appropriate.
+    if (isInternalHref(href)) {
+        if (href.split('').pop() === '/') {
+            href += 'index.html';
+        } else if (href.substring(href.length - 2) === '..') {
+            href += '/index.html';
+        } else if (href.indexOf('/#') >= 0) {
+            const index = href.indexOf('#');
+            href = href.substring(0, index) + 'index.html' + href.substring(index);
+        }
+    }
 
-        //  Has this link already been validated?
-        if (alreadyChecked.has(href)) {
-            //  Was it valid?
-            if (isValid.has(href)) {
+    const split = href.split('#');
+    if (split.length === 2) {
+        href = split[0] + '#' + decodeURIComponent(split[1]);
+    }
+
+
+
+    return href;
+}
+
+/**
+ * @typedef {Object} ScanDirectoryResult
+ * @property {Map<string, string>} internalLinks - Map of internal links.
+ * @property {Map<string, string>} internalAnchoredLinks - Map of internal anchored links.
+ * @property {Map<string, string>} internalParentLinks - Map of internal parent links.
+ * @property {Map<string, string>} internalParentAnchoredLinks - Map of internal parent anchored links.
+ * @property {Map<string, string>} externalLinks - Map of external links.
+ * @property {Map<string, Set<string>>} internalAnchors - Map of internal anchors.
+ * @property {Set<string>} internalPages - Set of internal pages.
+ */
+
+/**
+ * Scans a given directory for HTML files and extracts internal and external links.
+ *
+ * @param {string} directory - The directory path to scan.
+ * @returns {ScanDirectoryResult} - An object containing various maps and sets representing internal and external links.
+ */
+async function scanDirectory(directory) {
+    const internalLinks = new Map();                    //  Links to internal files, without a # anchor.
+    const internalAnchoredLinks = new Map();            //  Links to internal files with a # anchor.
+    const internalParentLinks = new Map();              // 
+    const internalParentAnchoredLinks = new Map();      //
+    const externalLinks = new Map();                    //  Links to external http(s) paths without a # anchor.
+    const externalAnchoredLinks = new Map();            //  Links to external http(s) paths with a # anchor.
+    const internalAnchors = new Map();                  //  All # anchors for an internal file.
+    const internalPages = new Set();
+
+    debug('scanning directory... ', directory);
+
+    fs.readdirSync(directory, { recursive: true })
+        .forEach((file) => {
+            if (!file.endsWith('.html')) {
                 return;
             }
 
-            //  Not valid, so store the info
-            markLinkInFileInvalid(file, href, lineNumber);
-        }
+            const filePath = path.join(directory, file);
+            const fileContent = fs.readFileSync(filePath);
 
-        //  Is this an external or internal link?
-        const isExternal = href.startsWith("http");
+            internalPages.add(filePath);
 
-        if (isExternal) {
-            if (!href.startsWith("https")) {
-                markLinkInFileInvalid(file, href, lineNumber);
-            } else {
-                const url = new URL(href);
-                const req = https.request(url, (res) => {
-                    if (res.statusCode === 404) {
-                        markLinkInFileInvalid(file, href, lineNumber);
+            const $ = cheerio.load(fileContent);
+
+            //  Extract links from <a> tags.
+            const aTags = $('body').find('a');
+            aTags.each((index, element) => {
+                let href = ($(element).attr('href') || '').trim();
+                // const $this = $(this);
+                // let href = ($this.attr('href') || '').trim();
+
+                if (!href || href === '') { return; }           //  Ignore anchor tags with no href attribute
+                if (href === '#') { return; }                   //  Ignore hash only links
+                if (href === '.') { return; }                   //  Ignore self referental hrefs
+                if (href.startsWith('mailto:')) { return; }     //  Ignore mailto: links
+                if (href.startsWith('javascript:')) { return; } //  Ignore javascript: executable hrefs
+
+                href = formatHref(href);
+                const absoluteHref = path.join(directory, href);
+
+                if (isExternalHref(href)) {
+                    if (href.indexOf('#') >= 0) {
+                        externalAnchoredLinks.set(href, filePath);
+                    } else {
+                        externalLinks.set(href, filePath);
                     }
-                });
-            }
-        } else {
-            const linkPath = path.join(rootDir, href);
+                } else if (absoluteHref.startsWith('..')) {
+                    if (href.indexOf('#') >= 0) {
+                        internalParentAnchoredLinks.set(absoluteHref, filePath);
+                    } else {
+                        internalParentLinks.set(absoluteHref, filePath);
+                    }
+                } else if (href.indexOf('#') >= 0) {
+                    const absoluteAnchoredHref = (href.indexOf('#') === 0 ? filePath + href : absoluteHref);
+                    internalAnchoredLinks.set(absoluteAnchoredHref, filePath);
+                } else {
+                    internalLinks.set(absoluteHref, filePath);
+                }
+            });
 
-            if (linkPath.endsWith("/")) {
-                linkPath += "index.html";
-            }
+            //  Extract anchors (id or name) attributes from file
+            const anchors = $('html').find('[id], [name]');
+            anchors.each((index, element) => {
+                const anchor = $(element).attr('id') || $(element).attr('name');
+                const entry = internalAnchors.get(filePath) || new Set();
+                entry.add(anchor);
+                internalAnchors.set(filePath, entry);
+            })
+        });
 
-            if (!fs.existsSync(linkPath)) {
-                markLinkInFileInvalid(file, href, lineNumber);
-            }
+    return {
+        internalLinks: internalLinks,
+        internalAnchoredLinks: internalAnchoredLinks,
+        internalParentLinks: internalParentLinks,
+        internalParentAnchoredLinks: internalParentAnchoredLinks,
+        externalLinks: externalLinks,
+        internalAnchors: internalAnchors,
+        internalPages: internalPages,
+    };
+}
+
+/**
+ * Validates internal links against existing internal pages.
+ *
+ * @param {Map<string, string>} internalLinks - Map of internal links.
+ * @param {Set<string>} internalPages - Set of internal pages.
+ * @param {Array<Object>} errors - Array to store validation errors.
+ */
+async function validateInternalLinks(internalLinks, internalPages, errors) {
+    internalLinks.forEach((sourcePage, link) => {
+        if (!internalPages.has(link)) {
+            errors.push({
+                type: 'page',
+                target: link,
+                source: sourcePage,
+                reason: 'page not found'
+            });
         }
-
-        isValid.add(href);
-        alreadyChecked.add(href);
     });
 }
 
-function linkChecker(rootDir) {
-    const opts = { recursive: true };
-    fs.readdirSync(rootDir, opts)
-        .filter((file) => file.endsWith(".html"))
-        .forEach((file) => validateFile(rootDir, file));
-
-    if (errors.length > 0) {
-        errors.forEach((err) => console.log(JSON.stringify(err)));
-    }
+/**
+ * Validates internal anchored links against existing anchors in internal pages.
+ *
+ * @param {Map<string, string>} internalAnchoredLinks - Map of internal anchored links.
+ * @param {Map<string, Set<string>>} internalAnchors - Map of internal anchors.
+ * @param {Array<Object>} errors - Array to store validation errors.
+ */
+async function validateInternalAnchoredLinks(internalAnchoredLinks, internalAnchors, errors) {
+    internalAnchoredLinks.forEach((sourcePage, link) => {
+        const index = link.indexOf('#');
+        const page = link.substring(0, index) || sourcePage;
+        const anchor = link.substring(index + 1);
+        const entry = internalAnchors.get(page) || new Set();
+        if (!entry.has(anchor)) {
+            errors.push({
+                type: 'anchor',
+                target: page + '#' + anchor,
+                anchor: anchor,
+                source: sourcePage,
+                reason: 'anchor not found'
+            });
+        }
+    });
 }
 
+/**
+ * Validates internal parent links against existing pages in the specified directory.
+ *
+ * @param {string} directory - The directory path to validate against.
+ * @param {Map<string, string>} internalParentLinks - Map of internal parent links.
+ * @param {Array<Object>} errors - Array to store validation errors.
+ */
+async function validateInternalParentLinks(directory, internalParentLinks, errors) {
+    const targets = Array.from(internalParentLinks.keys());
+
+    await Promise.all(targets.map((target) => {
+        return new Promise((resolve) => {
+            fs.exists(path.resolve(directory, target), (result) => {
+                resolve(result);
+            });
+        });
+    })).then((results) => {
+        results.forEach((result, index) => {
+            const target = targets[index];
+            const source = internalParentLinks.get(target);
+            if (!result) {
+                errors.push({
+                    type: 'page',
+                    target: target,
+                    source: source,
+                    reason: 'page not found'
+                });
+            }
+        });
+    });
+}
+
+/**
+ * Validates internal parent anchored links against existing pages and anchors.
+ *
+ * @param {string} directory - The directory path to validate against.
+ * @param {Map<string, string>} internalParentAnchoredLinks - Map of internal parent anchored links.
+ * @param {Array<Object>} errors - Array to store validation errors.
+ */
+async function validateInternalParentAnchoredLinks(directory, internalParentAnchoredLinks, errors) {
+    const targets = Array.from(internalParentAnchoredLinks.keys());
+
+    await Promise.all(targets.map((target) => {
+        return new Promise((resolve, reject) => {
+            const filePath = target.split('#')[0];
+            const absolutePath = path.resolve(directory, filePath);
+            fs.readFile(absolutePath, 'utf-8', resolve);
+        });
+    })).then((results) => {
+        results.forEach((result, index) => {
+            const target = targets[index];
+            const source = internalParentAnchoredLinks.get(target);
+            if (result instanceof Error) {
+                return errors.push({
+                    type: 'page',
+                    target: target,
+                    source: source,
+                    reason: 'page not found'
+                });
+            }
+
+            if (!result) {
+                errors.push({
+                    type: 'anchor',
+                    target: target,
+                    source: source,
+                    reason: 'anchor not found'
+                });
+            }
+        });
+    });
+
+}
+
+/**
+ * Validates external links by making HEAD requests.
+ *
+ * @param {Map<string, string>} externalLinks - Map of external links.
+ * @param {Array<Object>} errors - Array to store validation errors.
+ */
+async function validateExternalLinks(externalLinks, errors) {
+    const targets = Array.from(externalLinks.keys());
+
+    await Promise.all(targets.map((target) => {
+        return axios.head(target, {
+            validateStatus: (status) => {
+                return status !== 404;
+            }
+        });
+    })).then((responses) => {
+        responses.forEach((response, index) => {
+            if (!response || !response.statusCode || !response.statusCode >= 200 || !response.statusCode < 300) {
+                const error = response;
+                const target = targets[index];
+                const source = externalLinks.get(target);
+                errors.push({
+                    type: 'external-link',
+                    target: target,
+                    source: source,
+                    reason: 'could not fetch external page: ' + error.toString() + ' (Code: ' + response.statusCode + ')'
+                });
+            }
+        })
+    }).catch((error) => {
+        console.log("An unexpected error has occured");
+        console.log(error.toString());
+    });
+}
+
+/**
+ * Performs link checking for a given directory and provides a callback with the results.
+ *
+ * @param {string} directory - The directory path to perform link checking.
+ * @param {Function} callback - Callback function to receive the results.
+ */
+async function linkChecker(directory, callback) {
+    const {
+        internalLinks,
+        internalAnchoredLinks,
+        internalParentLinks,
+        internalParentAnchoredLinks,
+        externalLinks,
+        internalAnchors,
+        internalPages
+    } = await scanDirectory(directory);
+
+    const errors = [];
+
+    await validateInternalLinks(internalLinks, internalPages, errors)
+    await validateInternalAnchoredLinks(internalAnchoredLinks, internalAnchors, errors);
+    await validateInternalParentLinks(directory, internalParentLinks, errors);
+    await validateInternalParentAnchoredLinks(directory, internalParentAnchoredLinks, errors);
+    await validateExternalLinks(externalLinks, errors);
+
+    callback({
+        errors,
+        internalLinks: internalLinks.size,
+        internalAnchoredLinks: internalAnchoredLinks.size,
+        internalParentLinks: internalParentLinks.size,
+        internalParentAnchoredLinks: internalParentAnchoredLinks.size,
+        externalLinks: externalLinks.size
+    });
+
+}
+
+/**
+ * @typedef {Object} LinkCheckerResult
+ * @property {Array<Object>} errors - Array of validation errors.
+ * @property {number} internalLinks - Count of internal links.
+ * @property {number} internalAnchoredLinks - Count of internal anchored links.
+ * @property {number} internalParentLinks - Count of internal parent links.
+ * @property {number} internalParentAnchoredLinks - Count of internal parent anchored links.
+ * @property {number} externalLinks - Count of external links.
+ */
+
+/**
+ * Exports the linkChecker function as the module's main export.
+ *
+ * @type {Function}
+ * @param {string} directory - The directory path to perform link checking.
+ * @param {Function} callback - Callback function to receive the results.
+ * @returns {LinkCheckerResult} - Results of the link checking.
+ */
 module.exports = linkChecker;
