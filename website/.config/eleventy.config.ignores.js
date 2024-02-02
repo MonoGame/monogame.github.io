54b12441ac68348a33ee5cf149248c49a2ca8b90
Christopher Whitley <aristurtledev@gmail.com>
2024-02-02 02:07:53 -0500

    Ignore all of the website directory if we are only working on api or articles.

--- website/.config/eleventy.config.ignores.js
+++ website/.config/eleventy.config.ignores.js
@@ -3,4 +3,8 @@ module.exports = function(config) {
     config.setUseGitIgnore(false);
     config.ignores.add('./website/public/css/**/*.css');
     config.ignores.add('./website/public/js/**/*.js');
+
+    if(process.env.GENERATE_WEBSITE === false) {
+        config.ignores.add('./website/**/*');
+    }
 }
