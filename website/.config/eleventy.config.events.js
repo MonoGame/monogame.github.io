600484c29d74903abb1f44a70deb3f76c90513bd
Christopher Whitley <aristurtledev@gmail.com>
2024-02-04 13:58:18 -0500

    Output newline to console
    Ensure that a newline is output to console when finished to seperate docfx output from 11ty output.

--- website/.config/eleventy.config.events.js
+++ website/.config/eleventy.config.events.js
@@ -8,6 +8,10 @@ const sitemap = require('./events/after/sitemap');
 /** @param {import("@11ty/eleventy").UserConfig} config */
 module.exports = function (config) {
     config.on('eleventy.before', async ({ dir, runMode, outputMode }) => {
-        await docfxBuilder().catch((reason) => console.log(reason));
-    })
+        await docfxBuilder().then(() => console.log())
+                            .catch((reason) => {
+                                console.log(reason)
+                                console.log();
+                            });
+    });
 }
