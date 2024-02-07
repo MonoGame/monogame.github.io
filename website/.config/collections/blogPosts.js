e64628aff3638ff0e07f7db274d70d71390adf8e
Thomas Altenburger <mister.helmut@gmail.com>
2024-02-07 21:29:36 +0100

    Revert "Improve maintainability of website (#91)"
    This reverts commit dcb95658ea49936af16993faa95f2bbf4b70ded4.

--- website/.config/collections/blogPosts.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-/** @param {import("@11ty/eleventy/src/TemplateCollection")} api */
-function blogPosts(api) {
-    return api.getFilteredByGlob('./website/content/blog/*.md').sort((a, b) => a.date - b.date);
-}
-
-module.exports = blogPosts;
