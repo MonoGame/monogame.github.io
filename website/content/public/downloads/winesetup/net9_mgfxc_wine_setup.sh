45a59a18b04d5f3a9afe86b73d2c1f5a85cb6d73
Harry <harry.cpp@outlook.com>
2025-03-15 07:43:34 -0400

    Fix wine setup scripts (#210)
    * Update wine setup scripts to create a symlink for wine64
    
    * Use stable dotnet 9 sdk
    
    * Add symlink to dotnet 9 as well :(

--- website/content/public/downloads/winesetup/net9_mgfxc_wine_setup.sh
+++ website/content/public/downloads/winesetup/net9_mgfxc_wine_setup.sh
@@ -50,7 +50,7 @@ eval "$WINEEXECUTABLE regedit crashdialog.reg"
 popd
 
 # get dotnet
-DOTNET_URL="https://builds.dotnet.microsoft.com/dotnet/Sdk/9.0.100-rc.1.24452.12/dotnet-sdk-9.0.100-rc.1.24452.12-win-x64.zip"
+DOTNET_URL="https://builds.dotnet.microsoft.com/dotnet/Sdk/9.0.104/dotnet-sdk-9.0.104-win-x64.zip"
 curl $DOTNET_URL --output "$SCRIPT_DIR/dotnet-sdk.zip"
 7z x "$SCRIPT_DIR/dotnet-sdk.zip" -o"$WINEPREFIX/drive_c/windows/system32/"
 
@@ -61,8 +61,17 @@ curl $FIREFOX_URL --output "$SCRIPT_DIR/firefox.exe"
 cp -f "$SCRIPT_DIR/firefox_data/core/d3dcompiler_47.dll" "$WINEPREFIX/drive_c/windows/system32/d3dcompiler_47.dll"
 
 # append MGFXC_WINE_PATH env variable
-echo -e "\nexport MGFXC_WINE_PATH=$HOME/.winemonogame" >> ~/.profile
-echo -e "\nexport MGFXC_WINE_PATH=$HOME/.winemonogame" >> ~/.zprofile
+echo -e "\nexport MGFXC_WINE_PATH=\"$HOME/.winemonogame\"" >> ~/.profile
+echo -e "\nexport MGFXC_WINE_PATH=\"$HOME/.winemonogame\"" >> ~/.zprofile
+
+if ! type "wine64" > /dev/null 2>&1
+then
+    echo -e "\nexport PATH=\"\$PATH:$HOME/.winemonogame\"" >> ~/.profile
+    echo -e "\nexport PATH=\"\$PATH:$HOME/.winemonogame\"" >> ~/.zprofile
+
+    WINEPATH=$(which $WINEEXECUTABLE)
+    ln -s "$WINEPATH" "$HOME/.winemonogame/wine64"
+fi
 
 # cleanup
 rm -rf "$SCRIPT_DIR"
